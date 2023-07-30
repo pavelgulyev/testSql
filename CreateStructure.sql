d6378f1e1d0e55db3eebcc7f6976becf1e84575b
katyabarkalova <guliaev.p2014@yandex.ru>
2023-07-30 15:34:05 +0300

    Update CreateStructure.sql

--- CreateStructure.sql
+++ CreateStructure.sql
@@ -1,15 +1,19 @@
-CREATE TABLE  Employees(
+CREATE TABLE  SKU(
   ID INT PRIMARY KEY IDENTITY,
   Name NVARCHAR(20),
-  Birthday date,
-  Email nvarchar(30),
-  Position nvarchar(30),
-  Department nvarchar(30)
+  Code AS ('s' + ID) PERSISTED UNIQUE  
 )
-/*2.1 dbo.SKU (ID identity, Code, Name)
-   2.1.1 Ограничение на уникальность поля Code
-   2.1.2 Поле Code вычисляемое: "s" + ID
-2.2 dbo.Family (ID identity, SurName, BudgetValue)
-2.3 dbo.Basket (ID identity, ID_SKU (внешний ключ на таблицу dbo.SKU), ID_Family (Внешний ключ на таблицу dbo.Family) Quantity, Value, PurchaseDate, DiscountValue)
-   2.3.1 Ограничение, что поле Quantity и Value не могут быть меньше 0
-  2.3.2 Добавить значение по умолчанию для поля PurchaseDate: дата добавления записи (текущая дата)*/
+CREATE TABLE  Family(
+  ID INT PRIMARY KEY IDENTITY,
+  SurName NVARCHAR(20),
+  BudgetValue REAL 
+)
+CREATE TABLE  Basket(
+  ID INT PRIMARY KEY IDENTITY,
+  ID_SKU INT REFERENCES SKU (ID),
+  ID_Family INT REFERENCES Family (ID),
+  Quantity INT CHECK(Quantity >0),
+  Value REAL CHECK(Value >0), 
+  PurchaseDate DATE DEFAULT GETDATE(), 
+  DiscountValue REAL
+)
