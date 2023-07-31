e8ed7dec60502deb8ac9b073a516467fab48aa5f
katyabarkalova <guliaev.p2014@yandex.ru>
2023-07-31 16:17:29 +0300

    Update dbo.udf_GetSKUPrice.sql

--- dbo.udf_GetSKUPrice.sql
+++ dbo.udf_GetSKUPrice.sql
@@ -7,14 +7,11 @@ RETURNS decimal(18, 2)
 AS
 BEGIN
     DECLARE @ret decimal(18, 2);
-    SELECT @ret = SUM(b.Quantity)/SUM(b.Quantity)
+    SELECT @ret = SUM(b.Value)/SUM(b.Quantity)
     FROM Basket b
     WHERE b.ID_SKU = @ID_SKU
     RETURN @ret;
 END;
 
-/*Создать функцию (на выходе: файл в репозитории dbo.udf_GetSKUPrice.sql в ветке Functions)
---3.1 Входной параметр @ID_SKU
-3.2 Рассчитывает стоимость передаваемого продукта из таблицы dbo.Basket по формуле
-   3.1.1 сумма Value по переданному SKU / сумма Quantity по переданному SKU
-3.3 На выходе значение типа decimal(18, 2)*/
+
+
