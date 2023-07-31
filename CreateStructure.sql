300d663f1563b3b80fef2d9ec53994ac225ff019
katyabarkalova <guliaev.p2014@yandex.ru>
2023-07-31 16:18:50 +0300

    Update CreateStructure.sql

--- CreateStructure.sql
+++ CreateStructure.sql
@@ -12,8 +12,13 @@ CREATE TABLE  Basket(
   ID INT PRIMARY KEY IDENTITY,
   ID_SKU INT REFERENCES SKU (ID),
   ID_Family INT REFERENCES Family (ID),
+<<<<<<< Updated upstream
   Quantity INT CHECK(Quantity >0),
   Value REAL CHECK(Value >0), 
+=======
+  Quantity decimal CHECK(Quantity >0),
+  Value INT CHECK(Value >0), 
+>>>>>>> Stashed changes
   PurchaseDate DATE DEFAULT GETDATE(), 
   DiscountValue REAL
 )
